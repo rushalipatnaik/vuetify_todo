f00dd74db95e65f32d27e9d4779a96f4f6646e76
Rushali <rpatnaik78@gmail.com>
2022-01-25 10:55:37 +0530

    template changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
+      v-model = "drawer"
+       app
+       >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vuetify-Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Best todo ever!!
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Vuetify Todo</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -21,6 +51,14 @@
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({
+       drawer: null,
+        items: [
+          { title: 'Todo', icon: 'mdi-view-dashboard' },
+   
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+     
+    }),
   }
 </script>
