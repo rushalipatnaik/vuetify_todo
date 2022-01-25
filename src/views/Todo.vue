56473e936e1938bdf022ef2573092c9350d3d20b
Rushali <rpatnaik78@gmail.com>
2022-01-25 18:06:25 +0530

    minor fix

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -69,19 +69,8 @@
               title: 'Wake up',
               done: false
 
-            },
-             {
-              id: 2,
-              title: 'Get Bananas',
-               done: false
-
-            },
-             {
-              id: 3,
-              title: 'Eat Bananas',
-               done: false
-
             }
+      
           
         ]
       }
