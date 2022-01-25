f8bb0f1ca58b2496cb48b08cfce744ff274712bf
Rushali <rpatnaik78@gmail.com>
2022-01-25 19:45:57 +0530

    project done and dusted

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about pa-6">
-    <h1>This is an about page</h1>
+    <h1>About the app:</h1><br>
+    <h3> As the name implies, the Vuetify Todo app keeps a track of your daily tasks-to-do list. It has checklists to mark a task finished, and also the option to delete a task.<br>
+    The app has been made using VueJS with Vuetify.
+
+        
+    </h3>
   </div>
 </template>
